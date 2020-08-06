8dfa5124dc625a20f86a30de541072e3149948aa
Daniel Mattox <dmattox10@gmail.com>
2020-08-06 10:25:58 -0400

    Added PouchDB

--- client/src/hooks/useUsers.js
+++ client/src/hooks/useUsers.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
+import PouchDB from 'pouchdb-browser'
 
 export const useUsers = () => {
 
     const [users, updateUsers] = useState([])
 
     useEffect(() => {
-
+        const userDB = new PouchDB('users')
     }, [])
 
     const addUser = data => {
